a8316abaeb75c67a0ceccfae7277a2f4ce3272d4
Roma <roma@gmail.com>
2026-03-22 02:26:01 +0300

    fix io undefined

--- server.js
+++ server.js
@@ -1,23 +1,21 @@
+const express = require("express")
+const http = require("http")
+const { Server } = require("socket.io")
+
+const app = express()
+const server = http.createServer(app)
+
+// ВОТ ЭТО ТЫ ЗАБЫЛ
+const io = new Server(server, {
+  cors: {
+    origin: "*"
+  }
+})
+
 io.on("connection", (socket) => {
-const express = require("express");
-const http = require("http");
-const { Server } = require("socket.io");
+  console.log("user connected")
+})
 
-const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
-    console.log("Пользователь подключился");
-
-    socket.on("chat message", (msg) => {
-        io.emit("chat message", msg);
-    });
-
-    socket.on("disconnect", () => {
-        console.log("Пользователь вышел");
-    });
-});
-const PORT = process.env.PORT || 3000;
-
-server.listen(PORT, () => {
-  console.log("Server running on port " + PORT);
-});
+server.listen(3000, () => {
+  console.log("server running")
+})
