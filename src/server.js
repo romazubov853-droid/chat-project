a8ce2def40ab562f2ae0b2637c571156dc3851a7
Roma <roma@gmail.com>
2026-03-22 15:40:31 +0300

    add chat

--- src/server.js
+++ src/server.js
@@ -1,24 +1,29 @@
 const express = require("express")
 const http = require("http")
 const { Server } = require("socket.io")
+const path = require("path")
 
 const app = express()
 const server = http.createServer(app)
 
-// ВОТ ЭТО ТЫ ЗАБЫЛ
 const io = new Server(server, {
   cors: {
     origin: "*"
   }
 })
 
+// 👇 подключаем фронт
+app.use(express.static(path.join(__dirname, "public")))
+
+// 👇 чат логика
 io.on("connection", (socket) => {
   console.log("user connected")
+
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg) // отправка всем
+  })
 })
 
 server.listen(3000, () => {
   console.log("server running")
-  app.get("/", (req, res) => {
-    res.send("Сервер работает 🚀")
-  })
 })
