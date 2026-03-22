91493de1a6934e5116b30dedb926b953f19b65cb
Roma <roma@gmail.com>
2026-03-22 19:37:37 +0300

    fix render crash

--- src/server.js
+++ src/server.js
@@ -39,6 +39,6 @@ io.on("connection", async (socket) => {
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log("Server started");
+server.listen(PORT, () => {
+  console.log("Server started on port", PORT);
 });
